ac5fdd2139527d9af8bffb45477ebe272ff4fe5d
Christophe Ramananjaona <christophe.ramananjaona@polytechnique.edu>
2022-10-05 23:15:59 +0200

    Modify read script

--- truffle/scripts/increment.js
+++ truffle/scripts/increment.js
@@ -5,19 +5,23 @@
   https://trufflesuite.com/docs/truffle/getting-started/writing-external-scripts
 */
 
-const SimpleStorage = artifacts.require("SimpleStorage");
+const Cashier = artifacts.require("Cashier");
 
 module.exports = async function (callback) {
-  const deployed = await SimpleStorage.deployed();
+  const deployed = await Cashier.deployed();
+  const networkID = deployed.constructor.network_id;
+  console.log(`Deployer: ${deployed.constructor.class_defaults.from}`);
+  console.log(`Deployment address: ${deployed.constructor._json.networks[networkID].address}`);
 
-  const currentValue = (await deployed.read()).toNumber();
-  console.log(`Current SimpleStorage value: ${currentValue}`);
+  /* Ca ne marche pas sur Milkomeda C1 Testnet
+  const currentValue = (await deployed.getBalanceContract()).toNumber();
+  console.log(`Current Cashier balance: ${currentValue}`); */
 
-  const { tx } = await deployed.write(currentValue + 1);
+  /* const { tx } = await deployed.write(currentValue + 1);
   console.log(`Confirmed transaction ${tx}`);
 
   const updatedValue = (await deployed.read()).toNumber();
-  console.log(`Updated SimpleStorage value: ${updatedValue}`);
+  console.log(`Updated Cashier value: ${updatedValue}`); */
 
   callback();
 };
